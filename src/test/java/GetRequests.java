9af26be55728cffccfb3b5cdd087a660a5e6e072
@afsanayusifovaa <afsanayusifovaa@gmail.com>
2025-04-15 17:40:42 +0400

    init commit

--- src/test/java/GetRequests.java
+++ src/test/java/GetRequests.java
@@ -15,7 +15,7 @@ public class GetRequests {
                 .header("Accept-Language", "tr-TR,tr;q=0.9,ru;q=0.8,az;q=0.7,en;q=0.6")
                 .header("Cache-Control", "no-cache, no-store, must-revalidate")
                 .header("Connection", "keep-alive")
-                .header("Cookie", "_ga_RT0H1D1EP9=GS1.1.1735133735.1.0.1735133735.0.0.0; _ym_uid=1724240319454500842; _ym_d=1735133736; _ga=GA1.2.1169286022.1735133736; accessToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0NjI1NjY0LCJleHAiOjE3NDQ2NjE2NjR9.zsBuI6-ofjd1_qiBlo4K7KTmMLkDst5nisRreJqDLGRJ7LUw-dF1hvU7Y6BUecbI5K9ln5iEhAgk51qP4Et95g; refreshToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0NjI1NjY0LCJleHAiOjE3NDQ2MjkyNjR9.RRphQ7LNMcyUJI0CpTp8aMBBuAprPM3Qxhk5TFAvVfEtqzv20Usr8bSzAHGFamWzT_mcea3BRPk912MErYXq5g")
+                .header("Cookie", "_ga_RT0H1D1EP9=GS1.1.1735133735.1.0.1735133735.0.0.0; _ym_uid=1724240319454500842; _ym_d=1735133736; _ga=GA1.2.1169286022.1735133736; accessToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0NzExOTQ3LCJleHAiOjE3NDQ3NDc5NDd9.EYnLjq8aNZn5dFepWUaYuM6gJRpX5QIRGPXVblcvm8CI9gRSgwi-LjGIrVq48rsJGLwQzUzX7EOVF3hqOPSvJw; refreshToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0NzExOTQ3LCJleHAiOjE3NDQ3MTU1NDd9.spMagtPzLPbetCNcSOb7KZMHS7LGL--HnMyDDWCWkBWL5DIaALQyqynYJmVXF6wDomOHACL9cXEao3u1BIDxbg")
                 .header("Expires", "0")
                 .header("Host", "test-ahik-registry.aist.group")
                 .header("Pragma", "no-cache")
@@ -47,7 +47,7 @@ public class GetRequests {
                 .header("Accept-Language", "tr-TR,tr;q=0.9,ru;q=0.8,az;q=0.7,en;q=0.6")
                 .header("Cache-Control", "no-cache, no-store, must-revalidate")
                 .header("Connection", "keep-alive")
-                .header("Cookie", "_ga_RT0H1D1EP9=GS1.1.1735133735.1.0.1735133735.0.0.0; _ym_uid=1724240319454500842; _ym_d=1735133736; _ga=GA1.2.1169286022.1735133736; accessToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0MTA3ODM1LCJleHAiOjE3NDQxNDM4MzV9.7n39pROXBo598NN_Pen9UQ2pAtF_YJwlxJ9G51HmEXKJRBj3LJtrboGnD5Q9hc0nH6D4jFSD5M6U10TFx4lGFQ")
+                .header("Cookie", "_ga_RT0H1D1EP9=GS1.1.1735133735.1.0.1735133735.0.0.0; _ym_uid=1724240319454500842; _ym_d=1735133736; _ga=GA1.2.1169286022.1735133736; accessToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0NzExOTQ3LCJleHAiOjE3NDQ3NDc5NDd9.EYnLjq8aNZn5dFepWUaYuM6gJRpX5QIRGPXVblcvm8CI9gRSgwi-LjGIrVq48rsJGLwQzUzX7EOVF3hqOPSvJw;")
                 .header("Expires", "0")
                 .header("Host", "test-ahik-registry.aist.group")
                 .header("Pragma", "no-cache")
@@ -79,7 +79,7 @@ public class GetRequests {
                 .header("Accept-Language", "tr-TR,tr;q=0.9,ru;q=0.8,az;q=0.7,en;q=0.6")
                 .header("Cache-Control", "no-cache, no-store, must-revalidate")
                 .header("Connection", "keep-alive")
-                .header("Cookie", "_ga_RT0H1D1EP9=GS1.1.1735133735.1.0.1735133735.0.0.0; _ym_uid=1724240319454500842; _ym_d=1735133736; _ga=GA1.2.1169286022.1735133736; accessToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0MTA3ODM1LCJleHAiOjE3NDQxNDM4MzV9.7n39pROXBo598NN_Pen9UQ2pAtF_YJwlxJ9G51HmEXKJRBj3LJtrboGnD5Q9hc0nH6D4jFSD5M6U10TFx4lGFQ")  // Cookie başlığı
+                .header("Cookie", "_ga_RT0H1D1EP9=GS1.1.1735133735.1.0.1735133735.0.0.0; _ym_uid=1724240319454500842; _ym_d=1735133736; _ga=GA1.2.1169286022.1735133736; accessToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0NzExOTQ3LCJleHAiOjE3NDQ3NDc5NDd9.EYnLjq8aNZn5dFepWUaYuM6gJRpX5QIRGPXVblcvm8CI9gRSgwi-LjGIrVq48rsJGLwQzUzX7EOVF3hqOPSvJw;")  // Cookie başlığı
                 .header("Expires", "0")
                 .header("Referer", "https://test-ahik-registry.aist.group/employer-registery")
                 .header("Sec-CH-UA", "\"Chromium\";v=\"134\", \"Not:A-Brand\";v=\"24\", \"Google Chrome\";v=\"134\"")
@@ -108,7 +108,7 @@ public class GetRequests {
                 .header("Accept-Language", "tr-TR,tr;q=0.9,ru;q=0.8,az;q=0.7,en;q=0.6")
                 .header("Cache-Control", "no-cache, no-store, must-revalidate")
                 .header("Connection", "keep-alive")
-                .header("Cookie", "_ga_RT0H1D1EP9=GS1.1.1735133735.1.0.1735133735.0.0.0; _ym_uid=1724240319454500842; _ym_d=1735133736; _ga=GA1.2.1169286022.1735133736; accessToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0MTA3ODM1LCJleHAiOjE3NDQxNDM4MzV9.7n39pROXBo598NN_Pen9UQ2pAtF_YJwlxJ9G51HmEXKJRBj3LJtrboGnD5Q9hc0nH6D4jFSD5M6U10TFx4lGFQ")
+                .header("Cookie", "_ga_RT0H1D1EP9=GS1.1.1735133735.1.0.1735133735.0.0.0; _ym_uid=1724240319454500842; _ym_d=1735133736; _ga=GA1.2.1169286022.1735133736; accessToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0NzExOTQ3LCJleHAiOjE3NDQ3NDc5NDd9.EYnLjq8aNZn5dFepWUaYuM6gJRpX5QIRGPXVblcvm8CI9gRSgwi-LjGIrVq48rsJGLwQzUzX7EOVF3hqOPSvJw;")
                 .header("Expires", "0")
                 .header("Host", "test-ahik-registry.aist.group")
                 .header("Pragma", "no-cache")
@@ -140,7 +140,7 @@ public class GetRequests {
                 .header("Accept-Language", "tr-TR,tr;q=0.9,ru;q=0.8,az;q=0.7,en;q=0.6")
                 .header("Cache-Control", "no-cache, no-store, must-revalidate")
                 .header("Connection", "keep-alive")
-                .header("Cookie", "_ga_RT0H1D1EP9=GS1.1.1735133735.1.0.1735133735.0.0.0; _ym_uid=1724240319454500842; _ym_d=1735133736; _ga=GA1.2.1169286022.1735133736; accessToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0MTA3ODM1LCJleHAiOjE3NDQxNDM4MzV9.7n39pROXBo598NN_Pen9UQ2pAtF_YJwlxJ9G51HmEXKJRBj3LJtrboGnD5Q9hc0nH6D4jFSD5M6U10TFx4lGFQ")
+                .header("Cookie", "_ga_RT0H1D1EP9=GS1.1.1735133735.1.0.1735133735.0.0.0; _ym_uid=1724240319454500842; _ym_d=1735133736; _ga=GA1.2.1169286022.1735133736; accessToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0NzExOTQ3LCJleHAiOjE3NDQ3NDc5NDd9.EYnLjq8aNZn5dFepWUaYuM6gJRpX5QIRGPXVblcvm8CI9gRSgwi-LjGIrVq48rsJGLwQzUzX7EOVF3hqOPSvJw;")
                 .header("Expires", "0")
                 .header("Host", "test-ahik-registry.aist.group")
                 .header("Pragma", "no-cache")
@@ -161,6 +161,100 @@ public class GetRequests {
                 .statusCode(200);
 
         String responseBody = response.getBody().asPrettyString();
+    }
+    @Test
+    public void getTradeID(){
+        Response response = given()
+                .header("Accept", "application/json, text/plain, */*")
+                .header("Accept-Encoding", "gzip, deflate, br, zstd")
+                .header("Accept-Language", "tr-TR,tr;q=0.9,ru;q=0.8,az;q=0.7,en;q=0.6")
+                .header("Cache-Control", "no-cache, no-store, must-revalidate")
+                .header("Connection", "keep-alive")
+                .header("Cookie", "_ga_RT0H1D1EP9=GS1.1.1735133735.1.0.1735133735.0.0.0; _ym_uid=1724240319454500842; _ym_d=1735133736; _ga=GA1.2.1169286022.1735133736; accessToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0NzExOTQ3LCJleHAiOjE3NDQ3NDc5NDd9.EYnLjq8aNZn5dFepWUaYuM6gJRpX5QIRGPXVblcvm8CI9gRSgwi-LjGIrVq48rsJGLwQzUzX7EOVF3hqOPSvJw;")
+                .header("Expires", "0")
+                .header("Host", "test-ahik-registry.aist.group")
+                .header("Pragma", "no-cache")
+                .header("Referer", "https://test-ahik-registry.aist.group/trade-union-registery")
+                .header("Sec-CH-UA", "\"Chromium\";v=\"134\", \"Not:A-Brand\";v=\"24\", \"Google Chrome\";v=\"134\"")
+                .header("Sec-CH-UA-Mobile", "?0")
+                .header("Sec-CH-UA-Platform", "\"Windows\"")
+                .header("Sec-Fetch-Dest", "empty")
+                .header("Sec-Fetch-Mode", "cors")
+                .header("Sec-Fetch-Site", "same-origin")
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36")
+                .when()
+                .get("https://test-ahik-registry.aist.group/registry-api/trade-union/117884");
+
+        response
+                .then()
+                .log().all()
+                .statusCode(200);
+
+        String responseBody = response.getBody().asPrettyString();
+
+    }
+
+    @Test
+    public void getEmployerID(){
+        Response response = given()
+                .header("Accept", "application/json, text/plain, */*")
+                .header("Accept-Encoding", "gzip, deflate, br, zstd")
+                .header("Accept-Language", "tr-TR,tr;q=0.9,ru;q=0.8,az;q=0.7,en;q=0.6")
+                .header("Cache-Control", "no-cache, no-store, must-revalidate")
+                .header("Connection", "keep-alive")
+                .header("Cookie", "_ga_RT0H1D1EP9=GS1.1.1735133735.1.0.1735133735.0.0.0; _ym_uid=1724240319454500842; _ym_d=1735133736; _ga=GA1.2.1169286022.1735133736; accessToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0NzExOTQ3LCJleHAiOjE3NDQ3NDc5NDd9.EYnLjq8aNZn5dFepWUaYuM6gJRpX5QIRGPXVblcvm8CI9gRSgwi-LjGIrVq48rsJGLwQzUzX7EOVF3hqOPSvJw;")
+                .header("Expires", "0")
+                .header("Host", "test-ahik-registry.aist.group")
+                .header("Pragma", "no-cache")
+                .header("Referer", "https://test-ahik-registry.aist.group/trade-union-registery")
+                .header("Sec-CH-UA", "\"Chromium\";v=\"134\", \"Not:A-Brand\";v=\"24\", \"Google Chrome\";v=\"134\"")
+                .header("Sec-CH-UA-Mobile", "?0")
+                .header("Sec-CH-UA-Platform", "\"Windows\"")
+                .header("Sec-Fetch-Dest", "empty")
+                .header("Sec-Fetch-Mode", "cors")
+                .header("Sec-Fetch-Site", "same-origin")
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36")
+                .when()
+                .get("https://test-ahik-registry.aist.group/registry-api/employer/349156");
+
+        response
+                .then()
+                .log().all()
+                .statusCode(200);
+
+        String responseBody = response.getBody().asPrettyString();
+
+    }
+    @Test
+    public void getMemberID(){
+        Response response = given()
+                .header("Accept", "application/json, text/plain, */*")
+                .header("Accept-Encoding", "gzip, deflate, br, zstd")
+                .header("Accept-Language", "tr-TR,tr;q=0.9,ru;q=0.8,az;q=0.7,en;q=0.6")
+                .header("Cache-Control", "no-cache, no-store, must-revalidate")
+                .header("Connection", "keep-alive")
+                .header("Cookie", "_ga_RT0H1D1EP9=GS1.1.1735133735.1.0.1735133735.0.0.0; _ym_uid=1724240319454500842; _ym_d=1735133736; _ga=GA1.2.1169286022.1735133736; accessToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0NzExOTQ3LCJleHAiOjE3NDQ3NDc5NDd9.EYnLjq8aNZn5dFepWUaYuM6gJRpX5QIRGPXVblcvm8CI9gRSgwi-LjGIrVq48rsJGLwQzUzX7EOVF3hqOPSvJw;")
+                .header("Expires", "0")
+                .header("Host", "test-ahik-registry.aist.group")
+                .header("Pragma", "no-cache")
+                .header("Referer", "https://test-ahik-registry.aist.group/trade-union-registery")
+                .header("Sec-CH-UA", "\"Chromium\";v=\"134\", \"Not:A-Brand\";v=\"24\", \"Google Chrome\";v=\"134\"")
+                .header("Sec-CH-UA-Mobile", "?0")
+                .header("Sec-CH-UA-Platform", "\"Windows\"")
+                .header("Sec-Fetch-Dest", "empty")
+                .header("Sec-Fetch-Mode", "cors")
+                .header("Sec-Fetch-Site", "same-origin")
+                .header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36")
+                .when()
+                .get("https://test-ahik-registry.aist.group/registry-api/member/9205337");
+
+        response
+                .then()
+                .log().all()
+                .statusCode(200);
+
+        String responseBody = response.getBody().asPrettyString();
+
     }
 
 
--- src/test/java/PutRequests.java
+++ src/test/java/PutRequests.java
@@ -3,26 +3,61 @@ import io.restassured.response.Response;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class PutRequests {
     @Test
-    public void putObjectid(){
+    public void putMemberID(){
         JSONObject requestBody = new JSONObject();
-        requestBody.put("name","Apple MacBook Pro 17");
-        JSONObject requestData = new JSONObject();
-        requestData.put("price",2991);
-        requestData.put("CPU Model","Intel core i9");
-        requestData.put("Hard Disk size","1 TB");
-        // requestData.put("color","silver");
-        requestData.put("year",2019);
+        requestBody.put("name", "Afsana");
+        requestBody.put("surname", "Yusifova");
+        requestBody.put("patronymic", "Person");
+        requestBody.put("pin", "HJK1254");
 
         Response response = RestAssured
                 .given()
+                .header("Cookie", "_ga_RT0H1D1EP9=GS1.1.1735133735.1.0.1735133735.0.0.0; _ym_uid=1724240319454500842; _ym_d=1735133736; _ga=GA1.2.1169286022.1735133736; accessToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0NzExOTQ3LCJleHAiOjE3NDQ3NDc5NDd9.EYnLjq8aNZn5dFepWUaYuM6gJRpX5QIRGPXVblcvm8CI9gRSgwi-LjGIrVq48rsJGLwQzUzX7EOVF3hqOPSvJw;")
                 .contentType("application/json")
                 .body(requestBody.toString())
                 .when()
-                .put("https://api.restful-api.dev/objects/ff808181932badb60195065210682b06");
-        response.prettyPrint();
+                .put("https://test-ahik-registry.aist.group/registry-api/member/9205337");
 
+        response.prettyPrint();
+        System.out.println(response.getStatusCode());
     }
+    @Test
+    public void putEmployerID(){
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("name", "AİSTGROUP LLC");
+
+        Response response = RestAssured
+                .given()
+                .header("Cookie", "_ga_RT0H1D1EP9=GS1.1.1735133735.1.0.1735133735.0.0.0; _ym_uid=1724240319454500842; _ym_d=1735133736; _ga=GA1.2.1169286022.1735133736; accessToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0NzExOTQ3LCJleHAiOjE3NDQ3NDc5NDd9.EYnLjq8aNZn5dFepWUaYuM6gJRpX5QIRGPXVblcvm8CI9gRSgwi-LjGIrVq48rsJGLwQzUzX7EOVF3hqOPSvJw;")
+                .contentType("application/json")
+                .body(requestBody.toString())
+                .when()
+                .put("https://test-ahik-registry.aist.group/registry-api/employer/349156");
+
+        response.prettyPrint();
+        System.out.println(response.getStatusCode());
+    }
+    @Test
+    public void putTradeID(){
+        JSONObject requestBody = new JSONObject();
+        requestBody.put("name", "Kalibrlənmə laboratoriyaları");
+
+        Response response = RestAssured
+                .given()
+                .header("Cookie", "_ga_RT0H1D1EP9=GS1.1.1735133735.1.0.1735133735.0.0.0; _ym_uid=1724240319454500842; _ym_d=1735133736; _ga=GA1.2.1169286022.1735133736; accessToken=eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJpbXJhbm92ZmFyaWRAZ21haWwuY29tIiwidXNlcklkIjoxLCJyb2xlcyI6WyJVc2VyIl0sImN1cnJlbnRfcm9sZSI6MSwiaWF0IjoxNzQ0NzExOTQ3LCJleHAiOjE3NDQ3NDc5NDd9.EYnLjq8aNZn5dFepWUaYuM6gJRpX5QIRGPXVblcvm8CI9gRSgwi-LjGIrVq48rsJGLwQzUzX7EOVF3hqOPSvJw;")
+                .contentType("application/json")
+                .body(requestBody.toString())
+                .when()
+                .put("https://test-ahik-registry.aist.group/registry-api/trade-union/117885");
+
+        response.prettyPrint();
+        System.out.println(response.getStatusCode());
+    }
+
+
 
 }
